9f5cfd4e7d62272161ffdc56441a26cfd1af3cf2
Ian Lewis <IanMLewis@gmail.com>
2024-11-09 21:16:44 +0900

    feat: Add crc16 check (#12)
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- reader_test.go
+++ reader_test.go
@@ -24,6 +24,203 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+func TestReader(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name string
+		data []byte
+
+		fname     string
+		fcomment  string
+		os        byte
+		chunkSize int64
+		offsets   []int64
+		bytes     []byte
+		newErr    error
+		readErr   error
+	}{
+		{
+			// NOTE: Curiously, dictzip will compress an empty file
+			//       but dictunzip will throw an error for empty files.
+			name: "empty file",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgNAME,     // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				// NAME // empty.txt
+				0x65, 0x6d, 0x70, 0x74, 0x79, 0x2e, 0x74, 0x78, 0x74, 0x0,
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+
+			fname:     "empty.txt",
+			bytes:     []byte{},
+			os:        0x3,
+			chunkSize: 58315,
+			offsets:   []int64{32},
+		},
+		{
+			name: "fcomment",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCOMMENT,  // FLG
+				0x67, 0x31, 0x2f, 0x67, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN = 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN = 6
+				0x1, 0x0, // VER = 1
+				0xcb, 0xe3, // CHLEN = 58315
+				0x0, 0x0, // CHCNT = 0
+
+				// COMMENT // fcomment.txt
+				0x66, 0x63, 0x6f, 0x6d, 0x6d, 0x65, 0x6e, 0x74, 0x2e, 0x74, 0x78, 0x74, 0x0,
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			fcomment:  "fcomment.txt",
+			bytes:     []byte{},
+			os:        0x3,
+			chunkSize: 58315,
+			offsets:   []int64{35},
+		},
+		{
+			name: "with crc16",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCRC,      // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				0xe3, 0xb2, // CRC16
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			bytes:     []byte{},
+			os:        0x3,
+			chunkSize: 58315,
+			offsets:   []int64{24},
+		},
+		{
+			name: "bad crc16",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCRC,      // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				0x00, 0x00, // CRC16
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			bytes:  []byte{},
+			newErr: ErrHeader,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			z, err := NewReader(bytes.NewReader(tc.data))
+			if diff := cmp.Diff(tc.newErr, err, cmpopts.EquateErrors()); diff != "" {
+				t.Fatalf("NewReader (-want, +got):\n%s", diff)
+			}
+			// Skip other tests in the event of an error.
+			if err != nil {
+				return
+			}
+			defer z.Close()
+
+			if diff := cmp.Diff(tc.fname, z.Name); diff != "" {
+				t.Errorf("Name (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.fcomment, z.Comment); diff != "" {
+				t.Errorf("Name (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.os, z.OS); diff != "" {
+				t.Errorf("OS (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.chunkSize, z.ChunkSize()); diff != "" {
+				t.Errorf("ChunkSize (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.offsets, z.Offsets()); diff != "" {
+				t.Errorf("Offsets (-want, +got):\n%s", diff)
+			}
+
+			b, err := io.ReadAll(z)
+			if diff := cmp.Diff(tc.readErr, err, cmpopts.EquateErrors()); diff != "" {
+				t.Fatalf("ReadAll (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.bytes, b); diff != "" {
+				t.Fatalf("ReadAll (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestReader_Read(t *testing.T) {
 	t.Parallel()
 
@@ -279,109 +476,5 @@ func TestReader_ReadAt(t *testing.T) {
 	// r.offset should still be at the beginning of the file.
 	if diff := cmp.Diff(int64(0), r.offset); diff != "" {
 		t.Fatalf("r.offset (-want, +got):\n%s", diff)
-	}
-}
-
-func TestReader_readFlg(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		name         string
-		header       []byte
-		expectedRead int
-		expectedFlg  byte
-		expectedErr  error
-	}{
-		{
-			name: "EXTRA",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "invalid header",
-			header: []byte{
-				hdrGzipID[0],
-				0x01, // NOTE: Invalid Header
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedErr:  errInvalidIDHeader,
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "unsupported cm",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				0x07, // NOTE: Unsupported compression method
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedErr:  errUnsupportedCompression,
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "unexpected EOF",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				// NOTE: too few bytes
-			},
-			expectedErr:  io.ErrUnexpectedEOF,
-			expectedRead: 9,
-			expectedFlg:  0,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			n, flg, err := readFlg(bytes.NewReader(tc.header))
-
-			if diff := cmp.Diff(tc.expectedErr, err, cmpopts.EquateErrors()); diff != "" {
-				t.Fatalf("readFlg (-want, +got):\n%s", diff)
-			}
-
-			if diff := cmp.Diff(tc.expectedRead, n); diff != "" {
-				t.Errorf("readFlg read (-want, +got):\n%s", diff)
-			}
-
-			if diff := cmp.Diff(tc.expectedFlg, flg); diff != "" {
-				t.Errorf("readFlg flag (-want, +got):\n%s", diff)
-			}
-		})
 	}
 }
