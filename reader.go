5515c731db1da376c37496512eaa230a2ff38641
Ian Lewis <ianmlewis@gmail.com>
2024-11-09 11:54:49 +0000

    feat: Add crc16 check
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- reader.go
+++ reader.go
@@ -20,16 +20,24 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"hash"
+	"hash/crc32"
 	"io"
+	"strings"
+	"time"
 )
 
+var DefaultChunkSize = int64(1)
+
 var (
-	errInvalidIDHeader        = errors.New("invalid ID header")
-	errMissingHeader          = errors.New("dictzip header data not found")
-	errUnsupportedCompression = errors.New("unsupported compression method")
-	errUnsupportedVersion     = errors.New("unsupported dictzip version")
-	errUnsupportedSeek        = errors.New("unsupported seek mode")
-	errNegativeOffset         = errors.New("negative offset")
+	// ErrChecksum indicates an invalid gzip CRC-32 checksum.
+	ErrChecksum = errors.New("dictzip: invalid checksum")
+
+	// ErrHeader indicates an error with gzip header data.
+	ErrHeader = errors.New("dictzip: invalid header")
+
+	errUnsupportedSeek = errors.New("dictzip: unsupported seek mode")
+	errNegativeOffset  = errors.New("dictzip: negative offset")
 )
 
 // readCloseResetter is an interface that wraps the io.ReadCloser and
@@ -40,21 +48,74 @@ type readCloseResetter interface {
 	flate.Resetter
 }
 
+// noEOF converts io.EOF into io.ErrUnexpectedEOF.
+func noEOF(err error) error {
+	if errors.Is(err, io.EOF) {
+		return io.ErrUnexpectedEOF
+	}
+	return err
+}
+
+// Extra encapsulates an EXTRA data field.
+type Extra struct {
+	SI1  byte
+	SI2  byte
+	Data []byte
+}
+
+// Header is the gzip file header.
+//
+// Strings must be UTF-8 encoded and may only contain Unicode code points
+// U+0001 through U+00FF, due to limitations of the gzip file format.
+type Header struct {
+	// Comment is the COMMENT header field.
+	Comment string
+
+	// Extra is the EXTRA data field.
+	Extra []*Extra
+
+	// ModTime is the MTIME modification time field.
+	ModTime time.Time
+
+	// Name is the NAME header field.
+	Name string
+
+	// OS is the OS header field.
+	OS byte
+
+	// ChunkSize is the size of uncompressed dictzip chunks.
+	chunkSize int64
+
+	// Offsets is a list of offsets to the compressed chunks in the file.
+	offsets []int64
+}
+
+// ChunkSize returns the dictzip uncompressed data chunk size.
+func (h Header) ChunkSize() int64 {
+	return h.chunkSize
+}
+
+// Offsets returns the dictzip offsets for compressed data chunks.
+func (h Header) Offsets() []int64 {
+	return h.offsets
+}
+
 // Reader implements [io.Reader] and [io.ReaderAt]. It provides random access
 // to the compressed data.
 type Reader struct {
+	// Header is the gzip header data and is valid after [NewReader] or
+	// [Reader.Reset].
+	Header
+
 	r io.ReadSeeker
 	z readCloseResetter
 
 	// offset is the offset into the uncompressed data.
 	offset int64
 
-	// chunkSize is the size of uncompressed chunks.
-	chunkSize int64
-
-	// offsets is a list of offsets to the compressed chunks in the underlying
-	// reader.
-	offsets []int64
+	// digest is the CRC-32 digest (IEEE polynomial).
+	// See RFC-1952 Section 2.3.1.
+	digest hash.Hash32
 }
 
 // NewReader returns a new dictzip [Reader] reading compressed data from the
@@ -92,7 +153,7 @@ func (z *Reader) Reset(r io.ReadSeeker) error {
 	}
 
 	// Read the first 10 bytes of the header.
-	_, chunkSize, offsets, err := readHeader(z.r)
+	_, chunkSize, offsets, err := z.readHeader()
 	if err != nil {
 		return err
 	}
@@ -181,12 +242,15 @@ func (z *Reader) readChunk(offset, size int64) ([]byte, error) {
 	chunkReadSize := size + (offset - chunkFileOffset)
 
 	buf := make([]byte, chunkReadSize)
-	_, err := z.z.Read(buf)
-	if err != nil {
-		return nil, fmt.Errorf("decompressing: %w", err)
+	n, err := z.z.Read(buf)
+
+	// Check if we read less bytes than the start of our read.
+	readStart := chunkReadSize - size
+	if int64(n) < readStart {
+		return nil, err
 	}
 
-	return buf[chunkReadSize-size:], nil
+	return buf[chunkReadSize-size : n], err
 }
 
 // gzip Header Values
@@ -197,8 +261,11 @@ func (z *Reader) readChunk(offset, size int64) ([]byte, error) {
 +---+---+---+---+---+---+---+---+---+---+
 */
 var (
-	// hdrGzipID is the gzip header values for bytes ID1 and ID2.
-	hdrGzipID = []byte{0x1f, 0x8b}
+	// hdrGzipID1 is the gzip header value for ID1
+	hdrGzipID1 = byte(0x1f)
+
+	// hdrGzipID2 is the gzip header value for ID2
+	hdrGzipID2 = byte(0x8b)
 
 	// hdrDeflateCM is the deflate CM (Compression method).
 	hdrDeflateCM = byte(0x08)
@@ -214,30 +281,39 @@ var (
 // bit 6 : reserved (ignored).
 // bit 7 : reserved	(ignored).
 var (
-	flgCRC     = byte(2)
-	flgEXTRA   = byte(4)
-	flgNAME    = byte(8)
-	flgCOMMENT = byte(16)
+	flgTEXT    = byte(1 << 0)
+	flgCRC     = byte(1 << 1)
+	flgEXTRA   = byte(1 << 2)
+	flgNAME    = byte(1 << 3)
+	flgCOMMENT = byte(1 << 4)
 )
 
 // readFlg reads and validates the gzip header, and returns the FLG byte.
-func readFlg(r io.Reader) (int, byte, error) {
+func (z *Reader) readFlg() (int, byte, error) {
 	head := make([]byte, 10)
-	n, err := io.ReadFull(r, head)
+	n, err := io.ReadFull(z.r, head)
 	if err != nil {
 		return n, 0, fmt.Errorf("reading dictzip header: %w", err)
 	}
 
-	// ID1, ID2 headers must be 31 and 139 respectively.
-	if !bytes.Equal(head[0:2], hdrGzipID) {
-		return n, head[3], fmt.Errorf("%w: %x", errInvalidIDHeader, head[0:2])
+	if head[0] != hdrGzipID1 || head[1] != hdrGzipID2 {
+		return n, head[3], fmt.Errorf("%w: ID1,ID2: %x", ErrHeader, head[0:2])
 	}
 
 	if head[2] != hdrDeflateCM {
-		return n, head[3], fmt.Errorf("%w: %x", errUnsupportedCompression, head[2])
+		return n, head[3], fmt.Errorf("%w: CM: %x", ErrHeader, head[2])
 	}
 
-	// NOTE: MTIME, XFL, OS are ignored.
+	// NOTE: The zero value for MTIME means that the modified time is not set.
+	if mtime := binary.LittleEndian.Uint32(head[4:8]); mtime > 0 {
+		z.Header.ModTime = time.Unix(int64(mtime), 0)
+	}
+
+	// NOTE: XFL (head[8]) is ignored.
+
+	z.Header.OS = head[9]
+
+	z.digest = crc32.NewIEEE()
 
 	return n, head[3], nil
 }
@@ -245,24 +321,26 @@ func readFlg(r io.Reader) (int, byte, error) {
 // readExtra parses the EXTRA header. It returns dictzip chunk size before //
 // compression (before compression all chunks have equal size), and a list of
 // chunk sizes after compression.
-func readExtra(r io.Reader) (int, int64, []int64, error) {
+func (z *Reader) readExtra() (int, int64, []int64, error) {
 	var totalRead int
 
 	// FEXTRA
 	buf := make([]byte, 2)
-	n, err := io.ReadFull(r, buf)
+	n, err := io.ReadFull(z.r, buf)
 	totalRead += n
 	if err != nil {
-		return totalRead, 0, nil, fmt.Errorf("reading EXTRA XLEN: %w", err)
+		return totalRead, 0, nil, fmt.Errorf("EXTRA XLEN: %w", err)
 	}
 	xlen := binary.LittleEndian.Uint16(buf)
+	z.digest.Write(buf)
 
 	extra := make([]byte, xlen)
-	n, err = io.ReadFull(r, extra)
+	n, err = io.ReadFull(z.r, extra)
 	totalRead += n
 	if err != nil {
 		return totalRead, 0, nil, fmt.Errorf("reading EXTRA: %w", err)
 	}
+	z.digest.Write(extra)
 
 	// NOTE: The EXTRA field could could contain multiple sub-fields.
 	var chunkSize int64
@@ -301,8 +379,10 @@ func readExtra(r io.Reader) (int, int64, []int64, error) {
 	}
 
 	if !foundRAField {
-		return totalRead, 0, nil, fmt.Errorf("%w: no RA EXTRA field", errMissingHeader)
+		return totalRead, 0, nil, fmt.Errorf("%w: no RA EXTRA field", ErrHeader)
 	}
+
+	// TODO: Set z.Extra field.
 
 	return totalRead, chunkSize, sizes, nil
 }
@@ -321,7 +401,7 @@ func readExtraSizes(r io.Reader) (int64, []int64, error) {
 	ver := binary.LittleEndian.Uint16(buf)
 
 	if ver != 1 {
-		return 0, nil, fmt.Errorf("%w: %d", errUnsupportedVersion, ver)
+		return 0, nil, fmt.Errorf("%w: unsupported version: %d", ErrHeader, ver)
 	}
 
 	// Read CHLEN
@@ -353,72 +433,98 @@ func readExtraSizes(r io.Reader) (int64, []int64, error) {
 	return int64(chlen), offsets, nil
 }
 
+// readString reads a null terminated string from r.
+func (z *Reader) readString() (int64, string, error) {
+	var totalRead int64
+	var b strings.Builder
+
+	strBuf := make([]byte, 512)
+	buf := make([]byte, 1)
+	for i := 0; ; i++ {
+		if i >= len(strBuf) {
+			return totalRead, b.String(), fmt.Errorf("%w: string header len exceeded", ErrHeader)
+		}
+
+		n, err := io.ReadFull(z.r, buf)
+		totalRead += int64(n)
+		if err != nil {
+			return totalRead, "", fmt.Errorf("reading name header: %w", err)
+		}
+		strBuf[i] = buf[0]
+
+		if buf[0] == 0 {
+			// NOTE: The CRC digest includes the zero byte null terminator.
+			z.digest.Write(strBuf[:i+1])
+
+			// Strings are ISO 8859-1, Latin-1 (RFC 1952, section 2.3.1).
+			s := make([]rune, 0, i)
+			for _, v := range strBuf[:i] {
+				s = append(s, rune(v))
+			}
+			return totalRead, string(s), nil
+		}
+	}
+}
+
 // readHeader reads the gzip header for dictzip specific headers and returns
 // offsets and blocksize used for random access.
-func readHeader(r io.Reader) (int64, int64, []int64, error) {
+func (z *Reader) readHeader() (int64, int64, []int64, error) {
 	var chunkSize int64
 	var sizes []int64
 	var startOffset int64
 
-	n, flg, err := readFlg(r)
+	n, flg, err := z.readFlg()
 	startOffset += int64(n)
 	if err != nil {
 		return startOffset, 0, nil, err
 	}
 
 	if flg&flgEXTRA == 0 {
-		return startOffset, 0, nil, fmt.Errorf("%w: no EXTRA field", errMissingHeader)
+		return startOffset, 0, nil, fmt.Errorf("%w: no EXTRA field", ErrHeader)
 	}
 
 	// Read the EXTRA field
-	n, chunkSize, sizes, err = readExtra(r)
+	n, chunkSize, sizes, err = z.readExtra()
 	startOffset += int64(n)
 	if err != nil {
 		return startOffset, 0, nil, err
 	}
 
-	// Skip the NAME
+	// Read the NAME field.
 	if flg&flgNAME != 0 {
-		buf := make([]byte, 1)
-		for {
-			n, err := io.ReadFull(r, buf)
-			startOffset += int64(n)
-			if err != nil {
-				return startOffset, 0, nil, fmt.Errorf("reading name header: %w", err)
-			}
-			if buf[0] == 0 {
-				break
-			}
+		n, fname, err := z.readString()
+		startOffset += int64(n)
+		if err != nil {
+			return startOffset, 0, nil, fmt.Errorf("reading NAME header: %w", err)
 		}
+		z.Name = fname
 	}
 
-	// Skip the COMMENT
+	// Read the COMMENT field.
 	if flg&flgCOMMENT != 0 {
-		buf := make([]byte, 1)
-		for {
-			n, err := io.ReadFull(r, buf)
-			startOffset += int64(n)
-			if err != nil {
-				return startOffset, 0, nil, fmt.Errorf("reading comment header: %w", err)
-			}
-			if buf[0] == 0 {
-				break
-			}
-		}
-	}
-
-	// Skip the CRC
-	// TODO(#6): Perform a CRC check
-	if flg&flgCRC != 0 {
-		buf := make([]byte, 2)
-		n, err := io.ReadFull(r, buf)
+		n, fcomment, err := z.readString()
 		startOffset += int64(n)
 		if err != nil {
 			return startOffset, 0, nil, fmt.Errorf("reading comment header: %w", err)
 		}
+		z.Comment = fcomment
 	}
 
-	// calculate the offsets
+	// Perform a CRC check.
+	if flg&flgCRC != 0 {
+		buf := make([]byte, 2)
+		n, err := io.ReadFull(z.r, buf)
+		startOffset += int64(n)
+		if err != nil {
+			return startOffset, 0, nil, fmt.Errorf("reading comment header: %w", err)
+		}
+		digest := binary.LittleEndian.Uint16(buf)
+		if digest != uint16(z.digest.Sum32()) {
+			return startOffset, 0, nil, fmt.Errorf("%w: bad CRC digest", ErrHeader)
+		}
+	}
+
+	// Calculate the dictzip offsets.
 	offsets := make([]int64, len(sizes)+1)
 	offsets[0] = startOffset
 	for i := 0; i < len(sizes); i++ {
--- reader_test.go
+++ reader_test.go
@@ -24,6 +24,246 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+/*
+func makeDictzip(h Header, addCRC bool, chunkSize int64, level int, data []byte) {
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	zbuf := make([]byte, 10)
+	zbuf[0] = hdrGzipID1
+	zbuf[1] = hdrGzipID2
+	zbuf[2] = hdrDeflateCM
+
+	// FLG
+	if addCRC {
+		zbuf[3] |= flgCRC
+	}
+	zbuf[3] |= flgEXTRA
+	if h.Name != "" {
+		zbuf[3] |= flgNAME
+	}
+	if h.Comment != "" {
+		zbuf[3] |= flgCOMMENT
+	}
+
+	binary.LittleEndian.PutUint32(zbuf[4:8], uint32(h.ModTime.Unix()))
+	// NOTE: XFL is zero.
+	zbuf[10] = h.OS
+
+	if _, err := buf.Write(zbuf); err != nil {
+		panic(err)
+	}
+
+	chunks := make([][]byte, 0)
+	offsets := make([]int64, 0)
+	var offset int64
+	for i := int64(0); i < int64(len(data)); i += chunkSize {
+		j := i + chunkSize
+		if j > int64(len(data)) {
+			j = int64(len(data))
+		}
+		chunkData := data[i:j]
+
+		chunk := make([]byte, 0)
+		fw, err := flate.NewWriter(bytes.NewBuffer(chunk), level)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err := fw.Write(chunkData)
+		if err != nil {
+			panic(err)
+		}
+		if err := fw.Flush(); err != nil {
+			panic(err)
+		}
+		if err := fw.Close(); err != nil {
+			panic(err)
+		}
+		newOffset
+		len(chunk)
+	}
+
+	// Write the RA extra field.
+	buf.WriteByte('R')
+	buf.WriteByte('A')
+
+	// Write the rest of extra fields.
+}
+*/
+
+func TestReader(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name string
+		data []byte
+
+		fname    string
+		fcomment string
+		bytes    []byte
+		newErr   error
+		readErr  error
+	}{
+		{
+			// NOTE: Curiously, dictzip will compress an empty file
+			//       but dictunzip will throw an error for empty files.
+			name: "empty file",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgNAME,     // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				// NAME // empty.txt
+				0x65, 0x6d, 0x70, 0x74, 0x79, 0x2e, 0x74, 0x78, 0x74, 0x0,
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+
+			fname: "empty.txt",
+			bytes: []byte{},
+		},
+		{
+			name: "fcomment",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCOMMENT,  // FLG
+				0x67, 0x31, 0x2f, 0x67, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN = 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN = 6
+				0x1, 0x0, // VER = 1
+				0xcb, 0xe3, // CHLEN = 58315
+				0x0, 0x0, // CHCNT = 0
+
+				// COMMENT // fcomment.txt
+				0x66, 0x63, 0x6f, 0x6d, 0x6d, 0x65, 0x6e, 0x74, 0x2e, 0x74, 0x78, 0x74, 0x0,
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			fcomment: "fcomment.txt",
+			bytes:    []byte{},
+		},
+		{
+			name: "with crc16",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCRC,      // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				0xe3, 0xb2, // CRC16
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			bytes: []byte{},
+		},
+		{
+			name: "bad crc16",
+			data: []byte{
+				// Header
+				hdrGzipID1,
+				hdrGzipID2,
+				hdrDeflateCM,
+				flgEXTRA | flgCRC,      // FLG
+				0x00, 0x00, 0x00, 0x00, // MTIME
+				0x2, // XFL
+				0x3, // OS
+
+				// EXTRA
+				0xa, 0x0, // XLEN // 10
+				0x52, 0x41, // 'R', 'A'
+				0x6, 0x0, // LEN // 6
+				0x1, 0x0, // VER // 1
+				0xcb, 0xe3, // CHLEN // 58315
+				0x0, 0x0, // CHCNT // 0
+
+				0x00, 0x00, // CRC16
+
+				0x3, 0x0, 0x0, // Empty deflate data.
+
+				0x0, 0x0, 0x0, 0x0, // CRC32
+				0x0, 0x0, 0x0, 0x0, // ISIZE
+			},
+			bytes:  []byte{},
+			newErr: ErrHeader,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			z, err := NewReader(bytes.NewReader(tc.data))
+			if diff := cmp.Diff(tc.newErr, err, cmpopts.EquateErrors()); diff != "" {
+				t.Fatalf("NewReader (-want, +got):\n%s", diff)
+			}
+			// Skip other tests in the event of an error.
+			if err != nil {
+				return
+			}
+			defer z.Close()
+
+			if diff := cmp.Diff(tc.fname, z.Name); diff != "" {
+				t.Errorf("Name (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.fcomment, z.Comment); diff != "" {
+				t.Errorf("Name (-want, +got):\n%s", diff)
+			}
+
+			b, err := io.ReadAll(z)
+			if diff := cmp.Diff(tc.readErr, err, cmpopts.EquateErrors()); diff != "" {
+				t.Fatalf("ReadAll (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.bytes, b); diff != "" {
+				t.Fatalf("ReadAll (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestReader_Read(t *testing.T) {
 	t.Parallel()
 
@@ -279,109 +519,5 @@ func TestReader_ReadAt(t *testing.T) {
 	// r.offset should still be at the beginning of the file.
 	if diff := cmp.Diff(int64(0), r.offset); diff != "" {
 		t.Fatalf("r.offset (-want, +got):\n%s", diff)
-	}
-}
-
-func TestReader_readFlg(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		name         string
-		header       []byte
-		expectedRead int
-		expectedFlg  byte
-		expectedErr  error
-	}{
-		{
-			name: "EXTRA",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "invalid header",
-			header: []byte{
-				hdrGzipID[0],
-				0x01, // NOTE: Invalid Header
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedErr:  errInvalidIDHeader,
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "unsupported cm",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				0x07, // NOTE: Unsupported compression method
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-			},
-			expectedErr:  errUnsupportedCompression,
-			expectedRead: 10,
-			expectedFlg:  flgEXTRA,
-		},
-		{
-			name: "unexpected EOF",
-			header: []byte{
-				hdrGzipID[0],
-				hdrGzipID[1],
-				hdrDeflateCM,
-				flgEXTRA,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				0x00,
-				// NOTE: too few bytes
-			},
-			expectedErr:  io.ErrUnexpectedEOF,
-			expectedRead: 9,
-			expectedFlg:  0,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			n, flg, err := readFlg(bytes.NewReader(tc.header))
-
-			if diff := cmp.Diff(tc.expectedErr, err, cmpopts.EquateErrors()); diff != "" {
-				t.Fatalf("readFlg (-want, +got):\n%s", diff)
-			}
-
-			if diff := cmp.Diff(tc.expectedRead, n); diff != "" {
-				t.Errorf("readFlg read (-want, +got):\n%s", diff)
-			}
-
-			if diff := cmp.Diff(tc.expectedFlg, flg); diff != "" {
-				t.Errorf("readFlg flag (-want, +got):\n%s", diff)
-			}
-		})
 	}
 }
